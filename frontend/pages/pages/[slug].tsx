ebc55cfdaa4ceb39cca01944cec2dcc19797d7a9
Alexandrei Delgadillo <alexandrei@capicua.com.uy>
2021-08-02 17:31:34 -0400

    adding center container content

--- frontend/pages/pages/[slug].tsx
+++ frontend/pages/pages/[slug].tsx
@@ -2,6 +2,7 @@ import Head from "next/head"
 import { useRouter } from "next/router"
 import Navbar from "../../components/Navbar";
 import {getPage, getPages } from "../../utils/api"
+import {Button,Grid, Typography} from '@material-ui/core';
 
 const Pages = ({ page }) => {
   const router = useRouter()
@@ -15,16 +16,49 @@ const Pages = ({ page }) => {
         <title>{page.Title} </title>
       </Head>
       <Navbar />
-      <div className="w-full p-5 flex flex-col justify-between">
-        <div>
-          <h4 className="mt-1 font-semibold text-lg leading-tight truncate text-gray-700">
-           {page.Description}
-          </h4>
-          <div className="mt-1 text-gray-600">{page.Title}</div>
-        </div>
-        </div>
+
+
+<div >
+      <Grid container spacing={3}>
+  
+  <Grid item xs={12} style={{textAlign: 'center'}}>
+  
+  <Typography variant="h1" >
+  {page.Title}
+    </Typography>
+        </Grid> 
+        <Grid item xs={12} style={{textAlign: 'center'}}>
+        <Typography variant="h4" >
+          {page.Description}
+          </Typography>
+        </Grid> 
+        <Grid item xs={12} style={{textAlign: 'center'}}>
+        
+        <Button variant="outlined" color="primary" >
+        $0 Raised of $1,100 Goal
+      </Button>
+        </Grid>
+        <Grid item xs={12} style={{textAlign: 'center'}} >
+        <Button variant="contained" color="secondary"  style={{...margin(10, 10, 10, 5) }} >
+        Donate
+      </Button>
+      <Button variant="outlined" color="primary" >
+        Learn More
+      </Button>
+        </Grid>
+      </Grid>
     </div>
+</div>
   )
+}
+
+function margin(a, b, c, d) {
+  return {
+    marginTop: a,
+    marginRight: b ? b : a,
+    marginBottom: c ? c : a,
+    marginLeft: d ? d : (b ? b : a)
+  }
 }
 
 export default Pages;
